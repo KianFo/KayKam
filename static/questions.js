5929028762e90813b5420b43a66d3214b0cb8d35
kian <kianof2018@gmail.com>
2021-10-16 19:42:03 +0000

    finally completed the quiz but not nav and style

---
+++ static/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which team has won a sextuple?",
+    answer: "Barcelona",
+    options: [
+      "Real Madrid",
+      "Barcelona",
+      "Reachmond",
+      "Inter Milan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the best barca's right back of all time?",
+    answer: "Dani Alves",
+    options: [
+      "Sergi Roberto",
+      "Oscar Mingueza",
+      "Serginho Dest",
+      "Dani Alves"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the GOAT?",
+    answer: "Leo Messi",
+    options: [
+      "Cristiano Penaldo",
+      "Pele",
+      "Diego Armando Maradona",
+      "Leo Messi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Messi has scored the most goals with which coach?",
+    answer: "Pep Guardiola",
+    options: [
+      "Ernesto Valverde",
+      "Pep Guardiola",
+      "Frank Rijkaard",
+      "Louis Enrique"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who has the most games played for barca record after Messi?",
+    answer: "Xavi Hernandes",
+    options: [
+      "Andres Iniesta",
+      "Sergio Bosquests",
+      "Xavi Hernandes",
+      "Dani Alves"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
