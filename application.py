dba95ad5b13cc06586439853ad4584a4976c077d
kamranfotovat <kamran.fotovvat2018@gmail.com>
2021-11-13 22:28:51 +0330

    url for

--- application.py
+++ application.py
@@ -409,32 +409,31 @@ def charge():
 
 @app.route("/shop", methods=["POST", "GET"])
 def shop():
-    number1 = 0
+
+    return render_template("shop.html")
+
+    
+
+@app.route("/product1", methods=["GET", "POST"])
+def product1():
+    if request.method == "POST":
+            number1 = 0
 
     if request.method == "POST1":
         id = request.form.get("id")
         user_id = session["user_id"]
 
         try:
-            length = len(db.execute("SELECT * FROM cart WHERE id=? AND user_id=?", id, user_id))
+            test_length = len(db.execute("SELECT * FROM cart WHERE id=? AND user_id=?", id, user_id))
+            return render_template("iner.hmtl", test_length=test_length)
         except:
-            return render_template("")
+            return render_template("test1.html")
 
         try:
             number1 += 1
             db.execute("INSERT INTO cart (user_id, id, number) VALUES (?, ?, ?)", user_id, id, number1)
         except:
             return render_template("test1")
-
-        return redirect("/shop")
-
-    else:
-        return render_template("shop.html")
-
-@app.route("/product1", methods=["GET", "POST"])
-def product1():
-    if request.method == "POST":
-        pass
 
     else:
         return render_template("product1.html")
